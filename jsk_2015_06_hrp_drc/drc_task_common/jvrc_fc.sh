551a6f7d2aca9add80e04df08f528a44f591c016
YuOhara <ohara@jsk.imi.i.u-tokyo.ac.jp>
2015-09-13 11:19:18 +0900

    locomotion for jvrc

--- jsk_2015_06_hrp_drc/drc_task_common/jvrc_fc.sh
+++ jsk_2015_06_hrp_drc/drc_task_common/jvrc_fc.sh
@@ -4,6 +4,6 @@ source `dirname ${0}`/fc_tmux_init.sh
 
 tmux-newwindow executive "sleep 1; roslaunch drc_task_common fc_executive.launch fc:=false"
 tmux-newwindow lookat "sleep 1; roslaunch drc_task_common lookat.launch"
-# tmux-newwindow locomotion "sleep 5; roslaunch drc_task_common locomotion.launch"
+tmux-newwindow locomotion "sleep 5; roslaunch drc_task_common locomotion.launch INPUT_POINT_CLOUD:=/multisense/organized_image_points2_color"
 tmux send-keys -t fc:tmp "exit" C-m
 tmux a -t fc
